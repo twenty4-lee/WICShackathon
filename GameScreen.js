a2ee90135903809da982608ddd72fadefe2dfb5f
twenty4-lee <twenty4.lee@gmail.com>
2023-04-22 13:50:31 -0500

    matchdone

--- GameScreen.js
+++ GameScreen.js
@@ -24,25 +24,57 @@ export default function GameScreen() {
   }, []);
 
   const handleCardPress = (number) => {
-    // TODO: implement card press logic
-    const newResult = [...result, number];
-    setResult(newResult);
-    const newCards = [...cards];
-    const cardIndex = newCards.indexOf(number);
-    if (cardIndex !== -1) {
+    let isValid = true;
+    if (typeof result[result.length - 1] !== 'number' && (number === '+' || number === '-' || number === '*' || number === '/')) {
+      // Show error message popup
+      alert("You should click a number first!");
+      isValid = false;
+    }
+    if (isValid) {
+      const newResult = [...result, number];
+      setResult(newResult);
+      const newCards = [...cards];
+      const cardIndex = newCards.indexOf(number);
+      if (cardIndex !== -1) {
         newCards.splice(cardIndex, 1);
         setCards(newCards);
-    }
-
+      }
+    }      
   };
 
+  const calculateResult = () => {
+    const expression = result.join('');
+    const answer = eval(expression);
+    setResult([answer]);
+  };
+
+  const checkResult = (calculatedResult, goalNumber) => {
+    if (calculatedResult === goalNumber) {
+      alert("Congratulations! You have reached the goal number.");
+    } else {
+      alert("Sorry, the calculated result does not match the goal number.");
+    }
+  };
+  
+  const handleSubmit = () => {
+    const calculatedResult = eval(result.join(''));
+    checkResult(calculatedResult, goalNumber);
+  };
+  
+  
+
   return (
-    <View style={styles.container}>
+  <View style={styles.container}>
     <View style={styles.resultContainer}>
-        {result.map((number, index) => (
-        <Text key={index} style={styles.resultText}>{number}</Text>
-        ))}
+  {result.map((number, index) => (
+    <Text key={index} style={styles.resultText}>{number}</Text>
+  ))}
+<View style={styles.submitBtnContainer}>
+    <TouchableOpacity style={styles.submitBtn} onPress={() => { calculateResult(); handleSubmit(); }}>
+        <Text style={styles.submitBtnText}>Submit</Text>
+    </TouchableOpacity>
         </View>
+</View>
       <Text style={styles.title}>Make {goalNumber} with the cards</Text>
       <View style={styles.cardContainer}>
         <TouchableOpacity style={styles.card}>
@@ -75,9 +107,9 @@ export default function GameScreen() {
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.card}
-          onPress={() => handleCardPress("x")}
+          onPress={() => handleCardPress("*")}
         >
-          <Text style={styles.cardText}>x</Text>
+          <Text style={styles.cardText}>*</Text>
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.card}
@@ -91,16 +123,34 @@ export default function GameScreen() {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white'
+      },
     resultContainer: {
         width: '100%',
         height: 50,
         backgroundColor: '#eee',
         marginBottom: 20,
-        flexDirection: 'row',
+        flexDirection: 'row', // add this
         alignItems: 'center',
         justifyContent: 'flex-start',
       },
-      
+      submitBtnContainer: {
+        marginLeft: 'auto',
+      },
+      submitBtn: {
+        backgroundColor: '#4CAF50',
+        borderRadius: 5,
+        padding: 10,
+      },
+      submitBtnText: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+      },
       
   container: {
     flex: 1,
@@ -135,4 +185,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
+  
 });
